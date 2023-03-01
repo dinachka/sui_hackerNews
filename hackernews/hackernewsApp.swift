42efc9e64abb2710848b4002d3a9055e17c53237
dina <dinachernykh1@gmail.com>
2023-03-01 12:36:32 +0400

    Initial Commit

---
+++ hackernews/hackernewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackernewsApp.swift
+//  hackernews
+//
+//  Created by Дина Черных on 01.03.23.
+//
+
+import SwiftUI
+
+@main
+struct hackernewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
